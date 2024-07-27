10d28631921fa1c7153f5141bc7ab822b75e4946
Sherif-jr <94845939+Sherif-jr@users.noreply.github.com>
2024-07-28 02:24:39 +0300

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,70 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
+import "react-native-reanimated";
+import Header from "@/components/UI/Header";
+import { Provider } from "react-redux";
+import store from "@/store";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import ThemeProviders from "@/providers/ThemeProviders";
+import * as TaskManager from "expo-task-manager";
+import * as Notification from "expo-notifications";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+TaskManager.defineTask("BACKGROUND_NOTIFICATION", () => {
+  return null;
+});
+Notification.registerTaskAsync("BACKGROUND_NOTIFICATION");
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const [configLoaded, setConfigLoaded] = useState(false);
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
-    if (loaded) {
+    if (loaded && configLoaded) {
       SplashScreen.hideAsync();
     }
-  }, [loaded]);
+  }, [loaded, configLoaded]);
 
-  if (!loaded) {
-    return null;
-  }
+  // if (!loaded && !configLoaded) {
+  //   return null;
+  // }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <Provider store={store}>
+      <GestureHandlerRootView>
+        <ThemeProviders onThemeLoaded={() => setConfigLoaded(true)}>
+          <Stack
+            screenOptions={{
+              animation: "ios",
+            }}
+          >
+            <Stack.Screen
+              name="(pages)/index"
+              options={{
+                headerShown: true,
+                header() {
+                  return <Header />;
+                },
+              }}
+            />
+
+            <Stack.Screen
+              name="(pages)/ListEdit"
+              options={{
+                title: "Edit List",
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+        </ThemeProviders>
+      </GestureHandlerRootView>
+    </Provider>
   );
 }
